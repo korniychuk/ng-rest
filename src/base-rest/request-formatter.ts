7c9cff3ae3d77704ea53c10109da36138b66770d
Anton Korniychuk <ancor.root@gmail.com>
2017-03-24 07:16:45 +0200

    - fix Request formatter replace appending Authorization header with setting the header

--- src/base-rest/request-formatter.ts
+++ src/base-rest/request-formatter.ts
@@ -64,7 +64,7 @@ export class RequestFormatter<T extends RestRequestData> extends BaseRequestForm
    */
   protected prepareHeaders(data: T): void {
     if (data.token) {
-      this.headers.append('Authorization', `Bearer ${data.token}`);
+      this.headers.set('Authorization', `Bearer ${data.token}`);
     }
   } // end prepareHeaders()
 
